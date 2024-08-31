73b5db3dae0a256eddd3c6650bd3fb415f76a478
Uday-kumar-06 <130353691+Uday-kumar-06@users.noreply.github.com>
2024-09-01 01:27:02 +0530

    Add files via upload

---
+++ ytproject/src/main/java/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email){
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
